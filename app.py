08e3d45171fcbb8752f859601a50c70152353b89
PRUTHVI BELGAONKAR <pruth@LAPTOP-4AF2E2F7>
2022-06-29 11:43:10 +0530

    Logger and Exception added Succesfully

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import sys
 from flask import Flask
+from housing.exception import HousingException
+from housing.logger import logging
+
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message) 
+        logging.info("We are testing Logging module")
+    return "CI/CD PIPELINE ESTABLISHED"
 
 if __name__=="__main__":
     app.run(debug=True)
