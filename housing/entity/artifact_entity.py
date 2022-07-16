59190afee9da81dc75cf2d83572e104ebe8a222e
PRUTHVI BELGAONKAR <pruth@LAPTOP-4AF2E2F7>
2022-07-16 23:46:06 +0530

    notebook updated

--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -1,8 +1,13 @@
 from collections import namedtuple
 from unicodedata import name
 
+from housing.component.data_transformation import DataTransformation
+
 DataIngestionArtifact = namedtuple("DataIngestionArtifact",
                         ["train_file_path","test_file_path","is_ingested","message"])
 
 DataValidationArtifact = namedtuple("DataValidationArtifact",
                          ["schema_file_path","report_file_path","report_page_file_path","is_validated","message"])
+
+DataTransformationArtifact = namedtuple("ataTransformationArtifact",
+                        ["is_transformed","message","transformed_train_file_path","transformed_test_file_path","preprocessed_object_file_path"])
